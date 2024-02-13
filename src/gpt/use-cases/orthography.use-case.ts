a167afa5edac71a8b534f6c60fed71b2e9bf1213
Facundo Herrera <facundhfed@gmail.com>
2024-02-13 15:49:27 +0100

    First messages to openAi

--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -1,11 +1,19 @@
+import OpenAI from 'openai';
+
 interface Options {
   prompt: string;
 }
 
-export const orthographyCheckUsecase = async (options: Options) => {
+export const orthographyCheckUsecase = async (
+  openai: OpenAI,
+  options: Options,
+) => {
   const { prompt } = options;
-  return {
-    prompt: prompt,
-    apikey: process.env.OPENAI_API_KEY,
-  };
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: 'system', content: 'Eeres un asistente muy útil' }],
+    model: 'gpt-3.5-turbo',
+  });
+  console.log(completion);
+
+  return completion.choices[0];
 };
